6a53ef425fd4bdb17d66bc4b8e1d41fa85af0ebe
wiltonabreu <wiltonabreuu@gmail.com>
2022-11-20 11:14:36 -0300

    criado pasta de classes

--- config.php
+++ config.php
@@ -10,8 +10,8 @@ $db = array(
 
 spl_autoload_register(function($nameClass){//incluir classe de outros diretorios
 
-   
-    $filename = $nameClass . ".php";
+    $dirClass = "class";
+    $filename = $dirClass . DIRECTORY_SEPARATOR . $nameClass . ".php";
 
     if (file_exists($filename)) {
         require_once $filename;
