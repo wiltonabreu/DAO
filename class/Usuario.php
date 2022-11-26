c437a1e5d7ec50acd419abf4431ea692b2492c34
wiltonabreu <wiltonabreuu@gmail.com>
2022-11-26 10:12:22 -0300

    metodos de lista

--- class/Usuario.php
+++ class/Usuario.php
@@ -3,6 +3,7 @@ class Usuario{
 
     private $idusuario;
     private $deslogin;
+    private $nome;
     private $dessenha;
     private $dtcadastro;
 
@@ -20,6 +21,13 @@ class Usuario{
         $this->deslogin =$value;
     }
 
+    public function getNome(){
+        return $this->nome;
+    }
+    public function setNome($value){
+        $this->nome =$value;
+    }
+
     public function getDessenha(){
         return $this->dessenha;
     }
@@ -34,6 +42,10 @@ class Usuario{
         $this->dtcadastro =$value;
     }
     
+//============================================================================
+    /*Metodo loadById
+        Metodo que carrega um usuário pelo seu Identificador
+    */
     public function loadById($id){
         
         $sql = new Sql();
@@ -45,21 +57,98 @@ class Usuario{
 
             $row = $result[0];
             $this->setIdusuario($row['idusuario']);
-            $this->setDeslogin($row['deslogin']); 
+            $this->setDeslogin($row['deslogin']);
+            $this->setNome($row['nome']);  
             $this->setDessenha($row['dessenha']);
             $this->setDtcadastro(new Datetime($row['dtcadastro'])); 
            
         }
     }
 
+    /*Fim loadById*/
+
+//============================================================================
+
+    /*Metodo Mágico __toString
+        Metodo que imprime um Objeto quando a classe é Instanciada
+    */
     public function __toString(){
         return json_encode(array(
             "idusuario"=>$this->getIdusuario(),
             "delogin"=>$this->getDeslogin(),
+            "nome"=>$this->getNome(),
             "dessenha"=>$this->getDessenha(),
             "dtcadastro"=>$this->getDtcadastro()->format("d-m-Y H:i:s")
         ));
     }
+     /*FIM Metodo Mágico __toString*/
+
+
+
+//============================================================================
+
+    /*Metodo Mágico getList
+        Metodo que imprime todos os usuários
+    */
+
+    public static function getList(){
+        
+        $sql = new Sql();
+        return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin;");
+        
+    }
+     /*FIM MetodogetList*/
+
+//============================================================================
+
+//============================================================================
+
+    /*Metodo  search
+        Metodo que imprime um usuário pelo seu nome
+    */
+
+    public static function search($login){
+        
+        $sql = new Sql();
+        return $sql->select("SELECT * FROM tb_usuarios  WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
+            ':SEARCH'=>"%" . $login . "%"
+        ));
+        
+    }
+     /*FIM Metodo search*/
+
+//============================================================================
+
+    /*Metodo  login
+        Metodo que imprime um usuário autenticado
+    */
+
+    public function login($login, $password){
+        
+        $sql = new Sql();
+        $result = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN  AND dessenha = :PASSWORD", array(
+            ":LOGIN"=>$login,
+            "PASSWORD"=>$password
+        ));
+
+        if(count($result) > 0){
+
+            $row = $result[0];
+            $this->setIdusuario($row['idusuario']);
+            $this->setDeslogin($row['deslogin']); 
+            $this->setNome($row['nome']);
+            $this->setDessenha($row['dessenha']);
+            $this->setDtcadastro(new Datetime($row['dtcadastro'])); 
+           
+        } else {
+            throw new Exception("LOGIN E OU SENHA INVÁLIDOS");
+            
+        }
+        
+    }
+     /*FIM Metodo search*/
+
+//============================================================================
 
 }
 
--- index.php
+++ index.php
@@ -1,11 +1,45 @@
 <?php
 require_once("config.php");
 
-$usuario = new Usuario();
+//Carrega 1 usuário
+//$usuario = new Usuario();
 
-$usuario->loadById(2);
+//Carregando um usuário pelo seu Id
+//$usuario->loadById(1);
+
+ //Imprimindo um objeto (O objeto da classe usuario é impresso pelo metodo __toString da classe Usuário)
+//echo $usuario;
+
+//===================================================================
+
+//Carrega 1 lista de usuario
+
+// O metodo getList é Estatico, sendo assim o mesmo não precisa ser Instanciado.
+
+//$usuarios = Usuario::getList();
+
+//echo json_encode($usuarios);
+
+
+
+//===================================================================
+
+//Carrega usuario pela string passada como parametro
+
+// O metodo search é Estatico, sendo assim o mesmo não precisa ser Instanciado.
+
+//$search = Usuario::search('ab');
+
+//echo json_encode($search);
+
+
+//===================================================================
+
+//Carrega usuario autenticado
+
+// O metodo login não pode ser estatico, porque ele vai estar amarrado à classe pelo $this
+$usuario = new Usuario();
+$usuario->login("maria","123456");
 
 echo $usuario;
- 
-
 ?>
